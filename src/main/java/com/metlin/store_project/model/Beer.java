25e5cd6eb68c7e28f5f2f1c3583e3f1584df08ad
Mikhail <michail.metlin@smartbics.com>
2019-12-02 14:36:32 +0300

    add classes

---
+++ src/main/java/com/metlin/store_project/model/Beer.java
@@ -0,0 +1,9 @@
+package com.metlin.store_project.model;
+
+public class Beer {
+    private String id;
+    private String name;
+    private String price;
+    private String description;
+    private Long quantity;
+}
---
+++ src/main/java/com/metlin/store_project/model/User.java
@@ -0,0 +1,8 @@
+package com.metlin.store_project.model;
+
+public class User {
+    private String id;
+    private String name;
+    private String surname;
+   // List<> productList;
+}
